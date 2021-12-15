755cc3c0754ef836a2bb1df9c02490162eacdfd9
danteking <danteking@aliyun.com>
2021-12-15 23:50:25 +0800

    ProgressBar

---
+++ ProgressBar/ProgressBar.c
@@ -0,0 +1,31 @@
+//#include <stdio.h>                                                                                                                            
+//#include <unistd.h>
+//#define NUM 101
+//int main()
+//{
+//  int count = NUM;
+//  while(count--)
+//  {
+//    int percent = NUM - count;
+//    int i = NUM - count;
+//    int j = count;
+//    printf("[");
+//    while(i--)
+//    {
+//      printf("#");
+//    }
+//    while(j--)
+//    {
+//      printf(" ");
+//    }
+//    if(percent < 101)
+//        printf("%d\%]",percent);
+//    else 
+//        printf("100%]");
+//    printf(" \r");
+//    fflush(stdout);
+//    sleep(0.5);
+//  }
+//  return 0;
+//}
+
