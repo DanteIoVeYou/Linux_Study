710c636afedb8b13cecb762d4fa5943a2ca926d7
danteking <danteking@aliyun.com>
2022-04-01 10:41:33 +0800

    Blockqueue

--- bitlinuxosnetworkclass/lesson27/BlockQueue.hpp
+++ bitlinuxosnetworkclass/lesson27/BlockQueue.hpp
@@ -1,2 +1,65 @@
 #include <iostream>
+#include <queue>
+#include <pthread.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <ctime>
+#define CAPACITY 10;
+using namespace std;
+template<class T>
+class BlockQueue {
+public:
+  BlockQueue()
+    :_cap(0)
+  {
+    pthread_mutex_init(&lock, nullptr);
+    pthread_cond_init(&have_space, nullptr);
+    pthread_cond_init(&have_data, nullptr);
+  }
+  
+  ~BlockQueue()
+  {
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&have_space);
+    pthread_cond_destroy(&have_data);
+  }
+
+  bool IsEmpty() {
+    return _cap == 0;
+  }
+
+  bool IsFull() {
+    return _cap == CAPACITY;
+  }
+
+  void Put(const T& in) {
+    pthread_mutex_lock(&lock);
+    while(IsFull()) {
+      pthread_cond_wait(&have_space, &lock);
+    }
+    _bq.push(in);
+    _cap++;
+    pthread_cond_signal(&have_data);
+    pthread_mutex_unlock(&lock);
+  }
+
+  void Get(T* out) {
+    pthread_mutex_lock(&lock);
+    while(IsEmpty()) {
+      pthread_cond_wait(&have_data, &lock);
+    }
+    *out = _bq.front();
+    _bq.pop();
+    _cap--;
+    pthread_cond_signal(&have_space);
+    pthread_mutex_unlock(&lock);
+  }
+
+private:
+  int _cap;
+  queue<T> _bq;
+  pthread_mutex_t lock;
+  pthread_cond_t have_space;
+  pthread_cond_t have_data;
+};
 
--- bitlinuxosnetworkclass/lesson27/main.cc
+++ bitlinuxosnetworkclass/lesson27/main.cc
@@ -1,1 +1,31 @@
 #include "BlockQueue.hpp"
+void* consumer(void* args) {
+  BlockQueue<int>* bq = (BlockQueue<int>*)args;
+  while(true) {
+    sleep(1);
+    int out;
+    bq->Get(&out);
+    cout << "consumer: " << out << endl;
+  }
+}
+void* productor(void* args) {
+  srand((unsigned)time(nullptr));
+  BlockQueue<int>* bq = (BlockQueue<int>*)args;
+  while(true) {
+    int in = rand() % 100;
+    bq->Put(in);
+    cout << "productor: " << in << endl;
+  }
+}
+int main()
+{
+  BlockQueue<int>* bq = new BlockQueue<int>;
+  pthread_t c;
+  pthread_t p;
+  pthread_create(&c, nullptr, consumer, (void*)bq);
+  pthread_create(&p, nullptr, productor, (void*)bq);
+  pthread_join(c, nullptr);
+  pthread_join(p, nullptr);
+  delete bq;
+  return 0;
+}
