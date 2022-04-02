02db3858f6eb8836cb0780012edddad843460f3a
danteking <danteking@aliyun.com>
2022-04-02 22:43:57 +0800

    get tickets

--- bitlinuxosnetworkclass/lesson26/get_ticket.cc
+++ bitlinuxosnetworkclass/lesson26/get_ticket.cc
@@ -6,15 +6,22 @@
 using namespace std;
 
 int tickets = 1000;
+pthread_mutex_t lock;
+struct arg{
+  int x;
+};
 void* routine(void* args) {
   while(true) {
+    pthread_mutex_lock(&lock);
     if(tickets > 0) {
       usleep(1000);
-      printf("thread NO.  0x%x get a ticket, %d left\n",  pthread_self(), tickets--);
+      printf("thread NO. %d 0x%x get a ticket, %d left\n", ((arg*)args)->x, pthread_self(), tickets--);
       usleep(1000);
+      pthread_mutex_unlock(&lock);
     }
     else {
       printf("0x%x qiut... ticket: %d\n", pthread_self(), tickets);
+      pthread_mutex_unlock(&lock);
       break;
     }
   }
@@ -22,12 +29,17 @@ void* routine(void* args) {
 }
 int main()
 {
+
+  pthread_mutex_init(&lock, nullptr);
+  arg* arg1 = new arg;
+  arg1->x = 1;
   pthread_t tids[NUM];
   for(int i = 0; i < NUM; i++) {
-    pthread_create(tids+i, nullptr, routine, nullptr);
+    pthread_create(tids+i, nullptr, routine, arg1);
   }
   for(int i = 0; i < NUM; i++) {
     pthread_join(tids[i], nullptr);
   }
+  pthread_mutex_destroy(&lock);
   return 0;
 }
