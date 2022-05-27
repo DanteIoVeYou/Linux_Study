04e459763fd452b7686cad7c94aeedb69d1eda09
danteking <danteking@aliyun.com>
2022-05-27 12:35:45 +0800

    add manual and default port modification

--- bitlinuxosnetworkclass/lesson47/poll_server.cpp
+++ bitlinuxosnetworkclass/lesson47/poll_server.cpp
@@ -1,7 +1,20 @@
 #include "poll_server.hpp"
-int main()
+#include "utils.hpp"
+int main(int argc, char* argv[])
 {
-  ns_poll_server::PollServer *srv = new ns_poll_server::PollServer(8081);
-  srv->Loop();
+  if(argc == 1) {
+    // 默认8080端口
+    ns_poll_server::PollServer *srv = new ns_poll_server::PollServer();
+    srv->Loop();
+  }
+  else if(argc == 2) {
+    // 使用自定义的端口
+    ns_poll_server::PollServer *srv = new ns_poll_server::PollServer(atoi(argv[1]));
+    srv->Loop();
+  }
+  else {
+    ns_utils::Manual(argv[0]);
+    return 17;
+  }
   return 0;
 }
---
+++ bitlinuxosnetworkclass/lesson47/utils.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+namespace ns_utils { 
+
+  void Manual(char* proc) {
+    printf("Manual:\n\t%s [port](default port: 8080)\n", proc);
+  }
+
+}
